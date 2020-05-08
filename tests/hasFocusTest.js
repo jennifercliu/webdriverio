dafbf49b6d544bf0e7868af666b72ed90cbe41d4
Jennifer Liu <jenniferliu@Jennifers-MacBook-Pro.local>
2020-05-07 18:11:20 -0700

    hasFocus p2, isEnabled, isSelected, isVisibleWithinViewport

--- tests/hasFocusTest.js
+++ tests/hasFocusTest.js
@@ -2,6 +2,13 @@ describe('Test Radio Buttons Page', function(){
 	browser.url("/Dropdown-Checkboxes-RadioButtons/index.html");
 
 	it('Should be able to focus on checkbox button elements', function(done){
+			
+		browser.setViewportSize({
+			width: 1200,
+			height: 800
+		})
+
+		browser.pause(2000);
 		browser.click("input[value='option-1']");
 		var checkboxButtonOne = browser.hasFocus("input[value='option-1']");
 		console.log('Checkbox button one has a value of: ' + checkboxButtonOne);
