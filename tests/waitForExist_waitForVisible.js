e33e8c9bf36dbe75a5e1b4e51b9aca92089087b1
Jennifer Liu <jenniferliu@Jennifers-MacBook-Pro.local>
2020-05-09 11:20:21 -0700

    waitForText, waitForExist, waitForVisible

---
+++ tests/waitForExist_waitForVisible.js
@@ -0,0 +1,24 @@
+beforeEach(function() {
+  browser.url("/Ajax-Loader/index.html");
+})
+describe('Validate whether the click me button is visible', function() {
+  it('Validate whether the click me button code is visible within the DOM', function(done) {
+
+	this.timeout(20000);
+	var clickMeButton = "//p[text()='CLICK ME!']";
+	browser.waitForExist(clickMeButton, 8000, false);
+
+
+  });
+  it('Validate whether the click me button appears once the Ajax loader fully loads', function(done) {
+
+  	this.timeout(20000);
+	var clickMeButton = "//p[text()='CLICK ME!']";
+	browser.waitForVisible(clickMeButton, 8000, false);
+	browser.pause(2000);
+	browser.click("//p[text()='CLICK ME!']");
+	browser.pause(3000);
+
+
+  });
+});
