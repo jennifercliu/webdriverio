73ce323fd2a73162ef3af002f792bed1ed1806bf
Jennifer Liu <jenniferliu@Jennifers-MacBook-Pro.local>
2020-05-10 13:20:19 -0700

    waitUntil, waitForValue, using external data

--- tests/contactUsTest.js
+++ tests/contactUsTest.js
@@ -1,17 +1,25 @@
+var request = require('sync-request');
+
 beforeEach(function(){
 	browser.url('/Contact-Us/contactus.html');
 })
+
 describe('Test contact us from webdriveruni', function(){
+	var res = request('GET', 'http://jsonplaceholder.typicode.com/posts/1/comments');
+
+	var contactUsDetails = JSON.parse(res.getBody().toString('utf8'));
 
 	beforeEach(function(){
 	console.log('inside the describe block!');
 })
 
+contactUsDetails.forEach(function(contactDetails) {
+
 	it('Should be able to submit a successful submission', function(){
 		browser.setValue("[name='first_name']",'Jen');
 		browser.setValue("[name='last_name']",'Noodle');
-		browser.setValue("[name='email']",'test@mail.com');
-		browser.setValue("[name='message']",'test');
+		browser.setValue("[name='email']", contactDetails.email);
+		browser.setValue("[name='message']",contactDetails.body);
 		browser.click("[type='submit']");
 		
 		var successfulContactConfirmation = browser.isExisting("h1");
@@ -20,7 +28,9 @@ describe('Test contact us from webdriveruni', function(){
 		var successfulSubmission = browser.getText("h1");
 		expect(successfulSubmission).to.equal('Thank You for your Message!');
 		console.log(successfulSubmission);
-	});
+		});
+
+});
 
 	it('Should not be able to submit a succesful submission missing email', function(){
 		browser.setValue("[name='first_name']",'Jen');
