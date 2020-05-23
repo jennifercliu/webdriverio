176fcb027b34ee45795cbce55abb678a6a77817d
Jennifer Liu <jenniferliu@Jennifers-MacBook-Pro.local>
2020-05-23 11:46:09 -0700

    upgrade to webdriverIO v5 - install new wdio cli, create new wdio cli file, conif new wdio cli file

--- wdio_backup.conf.js
+++ wdio_backup.conf.js
@@ -1,23 +1,15 @@
 var baseUrl;
 
 if(process.env.SERVER === 'prod') {
-    baseUrl = 'https://www.google.com';
+	baseUrl = 'https://www.google.com';
 } else {
-    baseUrl = "http://www.webdriveruniversity.com";
+	baseUrl = "http://www.webdriveruniversity.com";
 }
 
 var timeout = process.env.DEBUG ? 99999999 : 10000
 
 exports.config = {
-    //
-    // ====================
-    // Runner Configuration
-    // ====================
-    //
-    // WebdriverIO allows it to run your tests in arbitrary locations (e.g. locally or
-    // on a remote machine).
-    runner: 'local',
-    
+
     //
     // ==================
     // Specify Test Files
@@ -71,8 +63,16 @@ exports.config = {
     // ===================
     // Define all options that are relevant for the WebdriverIO instance here
     //
-    // Level of logging verbosity: trace | debug | info | warn | error
-    logLevel: 'trace',
+    // By default WebdriverIO commands are executed in a synchronous way using
+    // the wdio-sync package. If you still want to run your tests in an async way
+    // e.g. using promises you can set the sync option to false.
+    sync: true,
+    //
+    // Level of logging verbosity: silent | verbose | command | data | result | error
+    logLevel: 'silent',
+    //
+    // Enables colors for log output.
+    coloredLogs: true,
     //
     // Warns when a deprecated command is used
     deprecationWarnings: true,
@@ -80,6 +80,9 @@ exports.config = {
     // If you only want to run your tests until a specific amount of tests have failed use
     // bail (default is 0 - don't bail, run all tests).
     bail: 0,
+    //
+    // Saves a screenshot to a given path if a command fails.
+    screenshotPath: './errorShots/',
     //
     // Set a base URL in order to shorten url command calls. If your `url` parameter starts
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
@@ -119,10 +122,10 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-     services: ['selenium-standalone'],
+    services: ['selenium-standalone'],
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
-    // see also: http://webdriver.io/docs/frameworks.html
+    // see also: http://webdriver.io/guide/testrunner/frameworks.html
     //
     // Make sure you have the wdio adapter package for the specific framework installed
     // before running any tests.
@@ -130,14 +133,34 @@ exports.config = {
     //
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
-    // see also: http://webdriver.io/docs/dot-reporter.html
-    reporters: ['dot'],
+    // see also: http://webdriver.io/guide/reporters/dot.html
+    reporters: ['dot', 'allure'],
+
+    reporterOptions: {
+        /**
+        junit: {
+            outputDir: './reports/junit-results/'
+        },
+
+        json: {
+            outputDir: './reports/json-results/'
+        },
+
+        **/
+        allure: {
+        	outputDir: './reports/allure-results/',
+        	disableWebdriverStepsReporting: true,
+        	disableWebdriverScreenshotsReporting: false,
+        	useCucumberStepReporter: false
+
+        },
+    },
     //
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
     mochaOpts: {
-        ui: 'bdd',
-        timeout: timeout
+    	ui: 'bdd',
+    	timeout: timeout
     },
     //
     // =====
@@ -161,8 +184,10 @@ exports.config = {
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {Array.<String>} specs List of spec file paths that are to be run
      */
-    // beforeSession: function (config, capabilities, specs) {
-    // },
+     beforeSession: function (config, capabilities, specs) {
+     	const del = require('del');
+     	del(['allure-report', 'errorShots', 'reports']);
+     },
     /**
      * Gets executed before test execution begins. At this point you can access to all global
      * variables like `browser`. It is the perfect place to define custom commands.
@@ -170,8 +195,8 @@ exports.config = {
      * @param {Array.<String>} specs List of spec file paths that are to be run
      */
      before: function (capabilities, specs) {
-        expect = require('chai').expect;
-        should = require('chai').should();
+     	expect = require('chai').expect;
+     	should = require('chai').should();
      },
     /**
      * Runs before a WebdriverIO command gets executed.
@@ -200,13 +225,13 @@ exports.config = {
     // beforeHook: function () {
     // },
     /**
-     * Hook that gets executed _after_ a hook within the suite starts (e.g. runs after calling
+     * Hook that gets executed _after_ a hook within the suite ends (e.g. runs after calling
      * afterEach in Mocha)
      */
     // afterHook: function () {
     // },
     /**
-     * Function to be executed after a test (in Mocha/Jasmine) or a step (in Cucumber) starts.
+     * Function to be executed after a test (in Mocha/Jasmine) or a step (in Cucumber) ends.
      * @param {Object} test test details
      */
     // afterTest: function (test) {
@@ -234,8 +259,14 @@ exports.config = {
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {Array.<String>} specs List of spec file paths that ran
      */
-    // after: function (result, capabilities, specs) {
-    // },
+
+     /**
+     after: function (result, capabilities, specs) {
+     	var name = 'ERROR-chrome-' + Date.now();
+     	browser.saveScreenshot('./errorShots/' +  name + '.png');
+     },
+     **/
+     
     /**
      * Gets executed right after terminating the webdriver session.
      * @param {Object} config wdio configuration object
@@ -249,8 +280,7 @@ exports.config = {
      * @param {Object} exitCode 0 - success, 1 - fail
      * @param {Object} config wdio configuration object
      * @param {Array.<Object>} capabilities list of capabilities details
-     * @param {<Object>} results object containing test results
      */
-    // onComplete: function(exitCode, config, capabilities, results) {
+    // onComplete: function(exitCode, config, capabilities) {
     // }
 }
